0d7cae0ea7227cae6637cdcb186d4935f80c184c
xanstered <gzak2@edu.cdv.pl>
2023-12-21 13:25:51 +0100

    nwm serio

--- Assets/CoinCollection.cs
+++ Assets/CoinCollection.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinCollection : MonoBehaviour
 {
 
     private int Coin = 0;
 
-    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI CoinText;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Coin")
             Coin++;
-        coinText.text = "Coin: " + Coin.ToString();
+        CoinText.text = "Coin: " + Coin.ToString();
         Debug.Log(Coin);
         Destroy(other.gameObject);
     }
